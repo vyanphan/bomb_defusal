492a09ee363a06f215acc00cb33fa6d131da46e4
Vy An Phan <vyan.n.phan@gmail.com>
2018-04-20 00:17:57 -0700

    simple wires

---
+++ simple_wires.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# give prompt for wires
+
+# receive prompt
+input = ''
+
+# parse colors
+
+
+
+
+wires = []
+serial_num = ''
+
+if len(wires)==3:
+	if 'r' not in wires:
+        print("2nd")
+    elif wires[-1]=='w':
+        print("last")
+    elif wires.count('b')>1:
+        print("last blue")
+    else
+        print("last")
