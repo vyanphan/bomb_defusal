dc700bfc6306e047b8d0f812269681a36179c40c
vyanphan <vyan.n.phan@gmail.com>
2018-04-28 21:30:36 -0700

    button stuff

--- button.py
+++ button.py
@@ -7,6 +7,12 @@ def held_button():
 def prompt_button():
     quit = False
     while not quit:
-        
+        user_input = input("Color Label:\t").lower()
+        if user_input == "help":
+            print("  Red\tr\n  Blue\tb\n  Yellow\ty\n  White\tw\n  Black\tk\n")
+        elif user_input == "q":
+            quit = True
+
+
 print("Button")
 prompt_button()
