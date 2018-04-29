430d342c7a1fbd0aba3c31b83a325a0b6e867785
vyanphan <vyan.n.phan@gmail.com>
2018-04-28 20:51:25 -0700

    simple wires input loop

--- simple_wires.py
+++ simple_wires.py
@@ -4,6 +4,7 @@ import sys
 wires = []
 serial_num = ''
 last_digit = None
+quit = False
 
 def prompt_serial_num():
     global serial_num
@@ -13,50 +14,59 @@ def prompt_serial_num():
     if last_digit==None:
         last_digit = int(serial_num[-1:])
 
-wires = [c for c in input("Simple Wires:\t").lower()]
+def prompt_wires():
+    global quit
+    while not quit:
+        user_input = input("Simple Wires:\t").lower()
+        if user_input == "help":
+            print("Red\tr\nBlue\tb\nYellow\ty\nWhite\tw\nBlack\tk\n")
+        elif user_input == "q":
+            quit = True
+        else:
+            wires = [c for c in user_input]
+            if len(wires)==3:
+                if 'r' not in wires:
+                    print("2nd")
+                elif wires[-1]=='w':
+                    print("last")
+                elif wires.count('b')>1:
+                    print("last blue")
+                else:
+                    print("last")
 
-if len(wires)==3:
-    if 'r' not in wires:
-        print("2nd")
-    elif wires[-1]=='w':
-        print("last")
-    elif wires.count('b')>1:
-        print("last blue")
-    else:
-        print("last")
+            elif len(wires)==4:
+                serial_num()    
+                if wires.count('r')>1 and last_digit%2==1:
+                    print("last red")
+                elif wires[-1]=='y' and wires.count('r')==0:
+                    print("1st")
+                elif wires.count('b')==1:
+                    print("1st")
+                elif wires.count('y')>1:
+                    print("last")
+                else:
+                    print("2nd")
 
-elif len(wires)==4:
-    serial_num()    
-    if wires.count('r')>1 and last_digit%2==1:
-        print("last red")
-    elif wires[-1]=='y' and wires.count('r')==0:
-        print("1st")
-    elif wires.count('b')==1:
-        print("1st")
-    elif wires.count('y')>1:
-        print("last")
-    else:
-        print("2nd")
+            elif len(wires)==5:
+                prompt_serial_num()
+                if wires[-1]=='k' and last_digit%2==1:
+                    print("4th")
+                elif wires.count('r')==1 and wires.count('y')>1:
+                    print("1st")
+                elif wires.count('k')==0:
+                    print("2nd")
+                else:
+                    print("1st")
 
-elif len(wires)==5:
-    prompt_serial_num()
-    if wires[-1]=='k' and last_digit%2==1:
-        print("4th")
-    elif wires.count('r')==1 and wires.count('y')>1:
-        print("1st")
-    elif wires.count('k')==0:
-        print("2nd")
-    else:
-        print("1st")
+            elif len(wires)==6:
+                prompt_serial_num()
+                if wires.count('y')==0 and last_digit%2==1:
+                    print("3rd")
+                elif wires.count('y')==1 and wires.count('w')>1:
+                    print("4th")
+                elif wires.count('r')==0:
+                    print("last")
+                else:
+                    print("4th")
 
-elif len(wires)==6:
-    prompt_serial_num()
-    if wires.count('y')==0 and last_digit%2==1:
-        print("3rd")
-    elif wires.count('y')==1 and wires.count('w')>1:
-        print("4th")
-    elif wires.count('r')==0:
-        print("last")
-    else:
-        print("4th")
-
+prompt_wires()
