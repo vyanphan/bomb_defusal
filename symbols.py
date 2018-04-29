1159c5ae8187cf67f4a20fa3f7a581feee7a4598
vyanphan <vyan.n.phan@gmail.com>
2018-04-29 12:38:05 -0700

    symbols

---
+++ symbols.py
@@ -0,0 +1,32 @@
+'''
+copyright symbol                    c
+fancy H                             H
+curly o                             o
+O with beard                        O
+A with line/mountain                A
+
+forwards C with dot                 C.
+backwards C with dot                .)
+backwards E with 2 dots             E..
+Cyrillic zhje/back-to-back Ks       K
+Cyrillic I with tail/backwards N    N
+
+ae                                  ae
+lambda with slash                   L
+psi/trident                         Y
+omega                               U
+
+b with a cross                      b
+flat 6                              6
+snake/funky 3                       3
+not equals/railroad track           =
+paragraph symbol/backwards P        P
+upside down question mark           ?
+
+lightning bolt/sowilo               Z
+blank star                          *
+black star                          **
+cat/bar line triangle               cat
+comma W/butt                        W
+smiley face                         :)
+'''
