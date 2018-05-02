bd5e4cf2d175e1a78fea7a8d529853d589f0f8cd
vyanphan <vyan.n.phan@gmail.com>
2018-05-02 11:22:54 -0700

    morse setup

--- modules/complicated.py
+++ modules/complicated.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-#   lrb*        = light red blue star
-#   (1 1 1 1)   = 15 = D
-#   r*          = red star
-#   (0 1 0 1)   =  5 = C
-#
-#   > lrb* r* b*
-#   CUT 1 4 5 
+'''
+    lrb*        = light red blue star
+    (1 1 1 1)   = 15 = D
+    r*          = red star
+    (0 1 0 1)   =  5 = C
+ 
+    > lrb* r* b*
+    CUT 1 4 5 
+'''
 
 def prompt_complicated():
     pass
--- modules/morse.py
+++ modules/morse.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+'''
+morse_dict {'.-': A}
+freq_dict {'shell': '3.505 MHz'}
+'''
+
 def prompt_morse():
     pass
