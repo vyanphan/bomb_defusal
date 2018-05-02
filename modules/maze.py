1d730b0fa0b754873043938736d1e7e2c21b58a2
vyanphan <vyan.n.phan@gmail.com>
2018-05-02 11:47:56 -0700

    setup maze

--- modules/maze.py
+++ modules/maze.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+'''
+Only need 1 of the circle markers.
+program maze solver (backtracking DFS with heuristic: manhattan distance, choose shorter direction first)
+
+store maze picture as string
+store maze as array of (NSEW) = (+1, -1, +1, -1)
+if wall on S, e.g. (+1, 0, +1, -1)
+
+keep tree of already traversed positions
+
+
+return path and drawn path
+for pos in array
+    if array[pos]==2
+        picture[pos] = '.'
+'''
+
 def prompt_maze():
     pass
