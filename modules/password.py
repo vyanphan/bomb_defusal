6a54bc0cd1104835b4b3b153a30933fefe472648
vyanphan <vyan.n.phan@gmail.com>
2018-05-02 11:52:35 -0700

    setup pswd

--- modules/password.py
+++ modules/password.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+'''
+only need the 1st 3 positions
+
+<12345> <12345> <12345>
+pos0    pos1    pos2
+
+wordlist
+prev = wordlist
+curr = []
+
+curr = [p for p in prev if p[i] in pos<i>]
+prev = curr
+
+print prev
+'''
+
 def prompt_password():
     pass
     
