768d842766ea86c6c63b02914a4179403024b540
vyanphan <vyan.n.phan@gmail.com>
2018-05-02 11:05:39 -0700

    complicated

--- modules/complicated.py
+++ modules/complicated.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+#   lrb*        = light red blue star
+#   (1 1 1 1)   = 15 = D
+#   r*          = red star
+#   (0 1 0 1)   =  5 = C 
+
 def prompt_complicated():
     pass
