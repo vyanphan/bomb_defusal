55562a8ed9417ba33995e1d9e92fa3a51f754ea9
vyanphan <vyan.n.phan@gmail.com>
2018-05-02 11:10:34 -0700

    memory setup

--- modules/memory.py
+++ modules/memory.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+'''
+> Stage 1:
+    0   = display1
+    []  = buttons1
+    i   = prespos1
+    n   = preslab1
+  Stage 2:
+  Stage 3:
+  Stage 4:
+  Stage 5:
+'''
+
 def prompt_memory():
     pass
