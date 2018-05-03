59abc249ecd47ba7baa3d8018b745a307c389a0f
vyanphan <vyan.n.phan@gmail.com>
2018-05-03 00:49:08 -0700

    stage 12

--- modules/memory.py
+++ modules/memory.py
@@ -20,32 +20,49 @@ Will automatically reset all at stage 5.
 
 def stage1_solve(buttons):
     if buttons[0]=='1' or buttons[0]=='2':
-        return 2
+        return buttons[2], 2
     elif buttons[0]=='3':
-        return 3
+        return buttons[3], 3
     elif buttons[0]=='4':
-        return 4
+        return buttons[4], 4
 
 def stage2_solve(buttons):
-    pass
+    if buttons[0]=='1':
+        return '4', buttons.index('4')
+    elif buttons[0]=='2' or buttons[0]=='4':
+        return stages[1][1]
+    elif buttons[0]=='3':
+        return 1
 
 def stage3_solve(buttons):
-    pass
+    if buttons[0]=='1':
+    elif buttons[0]=='2':
+    elif buttons[0]=='3':
+    elif buttons[0]=='4':
 
 def stage4_solve(buttons):
-    pass
+    if buttons[0]=='1':
+    elif buttons[0]=='2':
+    elif buttons[0]=='3':
+    elif buttons[0]=='4':
 
 def stage5_solve(buttons):
-    pass
+    if buttons[0]=='1':
+    elif buttons[0]=='2':
+    elif buttons[0]=='3':
+    elif buttons[0]=='4':
 
 def memory_help(buttons):
-    pass
+    if buttons[0]=='1':
+    elif buttons[0]=='2':
+    elif buttons[0]=='3':
+    elif buttons[0]=='4':
 
-stages = {1: [[], 0, stage1_solve],
-          2: [[], 0, stage2_solve],
-          3: [[], 0, stage3_solve],
-          4: [[], 0, stage4_solve],
-          5: [[], 0, stage5_solve]}
+stages = {1: [[], 0, -1, stage1_solve],
+          2: [[], 0, -1, stage2_solve],
+          3: [[], 0, -1, stage3_solve],
+          4: [[], 0, -1, stage4_solve],
+          5: [[], 0, -1, stage5_solve]}
 
 def prompt_memory():
     quit = False
@@ -66,15 +83,14 @@ def prompt_memory():
                 print()
         elif user_input=="reset all":
             stage = 6
-
         else:
             s = stages[stage]
             s[0] = [c for c in user_input]
             if len(s[0])!=5:
                 print("  Invalid button input!")
             else:
-                s[1] = s[2](s[0])
-                print("      " + s[0][s[1]])
+                s[1], s[2] = s[3](s[0])
+                print("      " + s[1])
                 stage += 1
         
         if stage>5:
