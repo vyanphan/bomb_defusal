cc1bcaa0be8f0da56855dd07b80071297cd96db5
vyanphan <vyan.n.phan@gmail.com>
2018-05-03 00:28:23 -0700

    memory

--- modules/memory.py
+++ modules/memory.py
@@ -2,7 +2,7 @@
 
 '''
 <display><b><b><b><b>
-> Stage 1: 51234
+> Stage 1: 41234
     0   = display1
     []  = buttons1
     i   = prespos1
@@ -18,29 +18,44 @@ Will automatically reset all at stage 5.
 '''
 
 
-def stage1_solve():
+def stage1_solve(buttons):
+    if buttons[0]=='1' or buttons[0]=='2':
+        return buttons[2]
+    elif buttons[0]=='3':
+        return buttons[3]
+    elif buttons[0]=='4':
+        return buttons[4]
+
+def stage2_solve(buttons):
     pass
 
-def stage2_solve():
+def stage3_solve(buttons):
     pass
 
-def stage3_solve():
+def stage4_solve(buttons):
     pass
 
-def stage4_solve():
+def stage5_solve(buttons1):
     pass
 
-def stage5_solve():
+def memory_help(buttons):
     pass
 
-def memory_help():
-    pass
+stages = {1: stage1_solve,
+          2: stage2_solve,
+          3: stage3_solve,
+          4: stage4_solve,
+          5: stage5_solve}
 
 def prompt_memory():
     quit = False
     stage = 1
     while not quit:
-        user_input = input("  > {0:16}".format("Stage "+ str(stage) +":")).strip().lower()
+        if stage==1:
+            user_input = input("  > {0:16}".format("Stage "+ str(stage) +":")).strip().lower()
+        else:
+            user_input = input("    {0:16}".format("Stage "+ str(stage) +":")).strip().lower()
+
         if user_input=="q":
             quit = True
         elif user_input=="help":
@@ -48,14 +63,15 @@ def prompt_memory():
         elif user_input=="reset":
             stage = max(1, stage-1)
         elif user_input=="reset all":
-            pass
+            stage = 6
         else:
-            
-            
-
-
-            stage = min(5, stage+1)
+            buttons = [c for c in user_input]
+            if len(buttons)!=5:
+                print("  Invalid button input!")
+            else:
+                stages[stage](buttons)
+                stage += 1
         
-        if stage==5:
-            # reset all
+        if stage>5:
+            stage = 1
             print()
