b2c8cdee9ab84bf78b24051ebef509b00214b9f1
vyanphan <vyan.n.phan@gmail.com>
2018-05-04 00:39:35 -0700

    complicated wires

--- modules/complicated.py
+++ modules/complicated.py
@@ -11,22 +11,22 @@ from modules import globvars
     CUT 1 4 5 
 '''
 
-# actions = {'0000': True,
-#            '0001': True,
-#            '0010': globvars.get_last_digit()%2==0,
-#            '0011': False,
-#            '0100': globvars.get_last_digit()%2==0,
-#            '0101': True,
-#            '0110': globvars.get_last_digit()%2==0,
-#            '0111': globvars.has_parallel_port(),
-#            '1000': False,
-#            '1001': globvars.get_batteries()>1,
-#            '1010': globvars.has_parallel_port(),
-#            '1011': globvars.has_parallel_port(),
-#            '1100': globvars.get_batteries()>1,
-#            '1101': globvars.get_batteries()>1,
-#            '1110': globvars.get_last_digit()%2==0,
-#            '1111': False}
+actions = {'0000': True,
+           '0001': True,
+           '0010': globvars.get_last_digit()%2==0,
+           '0011': False,
+           '0100': globvars.get_last_digit()%2==0,
+           '0101': True,
+           '0110': globvars.get_last_digit()%2==0,
+           '0111': globvars.get_parallel_port(),
+           '1000': False,
+           '1001': globvars.get_batteries()>1,
+           '1010': globvars.get_parallel_port(),
+           '1011': globvars.get_parallel_port(),
+           '1100': globvars.get_batteries()>1,
+           '1101': globvars.get_batteries()>1,
+           '1110': globvars.get_last_digit()%2==0,
+           '1111': False}
 
 def complicated_help():
     pass
@@ -49,18 +49,18 @@ def translate(code):
 
 def prompt_complicated():
     quit = False
-    # while not quit:
-    #     user_input = input("  > {0:16}".format("Wire Codes:")).strip().lower()
-    #     if user_input=="q":
-    #         quit = True
-    #     elif user_input=="help":
-    #         complicated_help()
-    #     elif user_input=="reset":
-    #         globvars.set_serial_num(None)
-    #         globvars.set_last_digit(None)
-    #         globvars.set_batteries(None)
-    #         globvars.set_parallel_port(None)
-    #     else:
-    #         wires = [translate(c) for c in user_input.split(' ')]
-    #         print("  " + ' '.join(wires))
-    #     print()
+    while not quit:
+        user_input = input("  > {0:16}".format("Wire Codes:")).strip().lower()
+        if user_input=="q":
+            quit = True
+        elif user_input=="help":
+            complicated_help()
+        elif user_input=="reset":
+            globvars.set_serial_num(None)
+            globvars.set_last_digit(None)
+            globvars.set_batteries(None)
+            globvars.set_parallel_port(None)
+        else:
+            wires = [translate(c) for c in user_input.split(' ')]
+            print("  " + ' '.join(wires))
+        print()
--- modules/globvars.py
+++ modules/globvars.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-serial_num = None
-last_digit = None
-batteries  = None
-indicator  = None
-strikes    = 0
+serial_num    = None
+last_digit    = None
+batteries     = None
+indicator     = None
+strikes       = 0
+parallel_port = None
 
 all_vars = [serial_num, last_digit, batteries, indicator]
 
@@ -58,11 +59,29 @@ def set_indicator(i):
     indicator = i
 
 def get_strikes():
+    global strikes
     return strikes
 
 def set_strikes(i):
     global strikes
     strikes = i
+
+def get_parallel_port():
+    global parallel_port
+    if parallel_port==None:
+        user_input = input("    {0:16}".format("Parallel port:")).strip().lower()
+        if user_input=='y' or user_input=='yes' or user_input=='t' or user_input=='true':
+            parallel_port = True
+        elif user_input=='n' or user_input=='no' or user_input=='f' or user_input=='false':
+            parallel_port = False
+        else
+            parallel_port = None
+            print("  Input must be yes or no!")
+    return parallel_port
+
+def set_parallel_port(i):
+    global parallel_port
+    parallel_port = i
 
 def reset_all():
     for var in all_vars:
