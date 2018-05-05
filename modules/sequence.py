743370bdbe3d6920e7ebb9cfe0e4ed5987a8a2fd
vyanphan <vyan.n.phan@gmail.com>
2018-05-05 00:07:31 -0700

    sequence wires setup

--- modules/sequence.py
+++ modules/sequence.py
@@ -43,7 +43,38 @@ black = [[1, 1, 1],
          [0, 0, 1],
          [0, 0, 1]]
 
+numRed   = 0
+numBlue  = 0
+numBlack = 0
 
+toNum = {'a':0, 'b':1, 'c':2}
+
+def translate(c):
+    if len(c)!=2:
+        print("  Invalid wire code!")
+    else:
+        if c[0]=='r':
+            numRed += 1
+            return red[numRed-1][toNum[c[1]]]
+        elif c[0]=='b':
+            numBlue += 1
+            return blue[numBlue-1][toNum[c[1]]]
+        elif c[0]=='k':
+            numBlack += 1
+            return black[numBlack-1][toNum[c[1]]]
+
+def sequence_help():
+    pass
 
 def prompt_sequence():
-    pass
+    quit = False
+    while not quit:
+        user_input = input("  > {0:16}".format("Wire Codes:")).strip().lower()
+        if user_input=="q":
+            quit = True
+        elif user_input=="help":
+            sequence_help()
+        else:
+            wires = [translate(c) for c in user_input.split(' ')]
+            print("  " + ' '.join(wires))
+        print()
