1ad29985fa5297f6728d39bec6222f8bf434eb20
vyanphan <vyan.n.phan@gmail.com>
2018-05-05 00:41:46 -0700

    setup knob

--- modules/knob.py
+++ modules/knob.py
@@ -6,14 +6,21 @@ relative positions
 
 Read these positions
 
-. . 1 3 4 .
-. . 2 . . .
+. . 2 3 4 .
+. . 1 . . .
 
 1101       = up
 1100, 0101 = down
 0001       = left
 1111, 1110 = right
 '''
+
+pos_dict = {'1101': 'UP',
+            '1100': 'DOWN',
+            '0101': 'DOWN',
+            '0001': 'LEFT',
+            '1111': 'RIGHT',
+            '1110': 'RIGHT'}
 
 def prompt_knob():
     pass
